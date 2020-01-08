edd6c7ce1d83af70fde9b14614ed33f1f1a99177
maggiefanss <55470289+maggiefanss@users.noreply.github.com>
2020-01-08 15:46:31 +0800

    01

---
+++ 105024204-2/Assets/scripts/eat.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class eat : MonoBehaviour
+{
+
+    public Text textCount;
+    public GameObject objTip;
+
+    public int propCount;
+    public int propTotal = 10;
+
+    public bool finish;
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject);
+        if(collision.gameObject.tag=="壽司")
+        {
+            Destroy(collision.gameObject);
+
+            propCount++;
+            textCount.text = "sushi" + propCount + "/10";
+            if(propCount==propTotal)
+            {
+                finish = true;
+                objTip.SetActive(true);
+            }
+        }
+    }
+    private void Update()
+    {
+        GameOver();
+    }
+    private void GameOver()
+    {
+        if(finish)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
---
+++ 105024204-2/Assets/scripts/player.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public string parRUN = "跑步開關";
+    public int countNeed = 10;
+    public int countCurrent;
+
+    public Animator ani;
+
+    private void Run()
+    {
+        print("水平" + Input.GetAxis("Horizontal"));
+        print("垂直" + Input.GetAxis("Vertical"));
+        
+        float h= Input.GetAxis("Horizontal");
+        float v= Input.GetAxis("Vertical");
+        ani.SetBool("跑步開關", h != 0 || v != 0);
+    }
+    private void Update()
+    {
+        Run();
+    }
+}
